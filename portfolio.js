eab93c2fbe64118c7e33a6bd925b8a8cd30edd03
malicxk <144208222+malicxk@users.noreply.github.com>
2023-09-15 09:51:17 +0530

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,39 @@
+console.log("hiiiiii")
+
+$(document).ready(function(){
+    
+    $("#formval").validate({
+        rules:{
+            name:{
+                required:true,
+                normalizer:function(value){
+                    return $.trim(value);
+                },
+                minlength:3
+                },
+                email:{
+                    required:true,
+                },
+                subject:{
+                    required:true,
+                    normalizer:function(value){
+                        return $.trim(value);
+                    },
+                    minlength:6,
+                },
+                message:{
+                    required:true,
+                    normalizer:function(value){
+                        return $.trim(value);
+                    },
+                    minlength:10,
+                },
+        }
+        
+    })
+});
+
+
+
+
+
